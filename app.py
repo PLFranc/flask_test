0ce52b28bbe950b19eaa660916a9b37031c39f61
PLFranc <pierre-louis.franc@epfedu.fr>
2021-02-01 19:32:59 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello():
+    return "Hello World 1"
+
+
+@application.route('/index')
+def index():
+    return "Hey This is an index page"
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
