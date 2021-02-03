1262585a2bca1c4d6313276123a2c1ed61486f02
PLFranc <pierre-louis.franc@epfedu.fr>
2021-02-03 12:14:55 +0100

    apparition des text

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ app = application
 def index():
     return render_template('index.html')
 
+@application.route('/pronoo')
+def pronoo():
+    return render_template('pronoo.html')
+
 
 if __name__ == '__main__':
     application.run(debug=True)
