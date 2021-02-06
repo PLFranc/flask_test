02259311a09862b387ba4b6e882e144500b21fcc
PLFranc <pierre-louis.franc@epfedu.fr>
2021-02-06 12:46:44 +0100

    connexion a la bdd

--- application.py
+++ application.py
@@ -1,6 +1,39 @@
 from flask import Flask, render_template
+from get_data import get_match
+
+
 application = Flask(__name__)
 app = application
+
+
+# class Match():
+#     __tablename__ = 'match'
+#     id = db.column(db.Integer,primary_key = True)
+#     tournoi = db.column(db.VARCHAR, unique = False)
+#     date = db.column(db.TIMESTAMP, unique = False)
+#     player_1_name = db.column(db.VARCHAR, unique = False)
+#     player_1_odd = db.column(db.REAL, unique = False)
+#     player_1_rank = db.column(db.Integer, unique = False)
+#     player_1_prediction = db.column(db.REAL, unique = False)
+#     player_2_name = db.column(db.VARCHAR, unique = False)
+#     player_2_odd = db.column(db.REAL, unique = False)
+#     player_2_rank = db.column(db.Integer, unique = False)
+#     player_2_prediction = db.column(db.REAL, unique = False)
+#
+#     def __init__(self, id, tournoi, date, player_1_name, player_1_odd, player_1_rank, player_1_prediction,
+#                  player_2_name, player_2_odd, player_2_rank, player_2_prediction):
+#         self.id = id
+#         self.tournoi = tournoi
+#         self.date = date
+#         self.player_1_name = player_1_name
+#         self.player_1_odd = player_1_odd
+#         self.player_1_rank = player_1_rank
+#         self.player_1_prediction = player_1_prediction
+#         self.player_2_name = player_2_name
+#         self.player_2_odd = player_2_odd
+#         self.player_2_rank = player_2_rank
+#         self.player_2_prediction = player_2_prediction
+
 
 
 @application.route('/')
@@ -9,8 +42,10 @@ def index():
 
 @application.route('/pronoo')
 def pronoo():
-    return render_template('pronoo.html')
+    matchs = get_match()
+    return render_template('pronoo.html', matchs = matchs)
 
 
 if __name__ == '__main__':
+
     application.run(debug=True)
---
+++ get_data.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+
+def get_match():
+    """ insert a new vendor into the vendors table """
+    sql = """SELECT * FROM match;"""
+    conn = None
+    id = None
+    try:
+        # read database configuration
+
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(user="postgres",
+                                  password="Jjci3kJy57b4GAsLWiUC",
+                                  host="db-1.ctjpvwq07ek9.us-east-2.rds.amazonaws.com",
+                                  port="5432",
+                                  database="postgres")
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql)
+        # commit the changes to the database
+        matchs = cur.fetchall()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            return matchs
